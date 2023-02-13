5060191c1d17a507fef6d333b29acfa7ad339d11
Kirabo Ibrahim <kiraboibra268@gmail.com>
2023-02-13 21:41:16 +0300

    feat: Watch for DOM changes to filter new text nodes

--- src/content.ts
+++ src/content.ts
@@ -8,14 +8,16 @@ class NSFWWordsFilter {
 
   constructor() {}
 
-  protected traverse_text_nodes_under(root: Node) {
-    let accept_node = (node: Node) => {
-      return ["SCRIPT", "STYLE"].includes((node.parentElement as Node).nodeName) || (node as CharacterData).data.replace(/\s/g, "").length  <= 1 ? NodeFilter.FILTER_REJECT : NodeFilter.FILTER_ACCEPT;
+  async watch() {
+    let callback = (mutation_list: MutationRecord[], observer: MutationObserver): void => {
+      for(let mutation of mutation_list) {
+        if(mutation.type == "childList" && mutation.addedNodes.length > 0)
+          this.filter(mutation.target);
+      }
     }
-    let tree_walker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT, {acceptNode: accept_node})
-    return tree_walker;
+    let observer: MutationObserver = new MutationObserver(callback);
+    observer.observe(document.body, {childList: true, subtree: true, characterData: true});
   }
-
   async filter(root: Node = document.body) {
     let text_node_walker = this.traverse_text_nodes_under(root);
     let text_node: Node | null = text_node_walker.nextNode();
@@ -34,10 +36,33 @@ class NSFWWordsFilter {
       text_node = next_text_node;
     }
   }
+
+  protected traverse_text_nodes_under(root: Node) {
+    let accept_node = (node: Node) => {
+      return NSFWWordsFilter.is_script_node(node) || NSFWWordsFilter.is_text_node_empty(node) || NSFWWordsFilter.is_text_node_checked(node) ? NodeFilter.FILTER_REJECT : NodeFilter.FILTER_ACCEPT;
+    }
+    let tree_walker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT, {acceptNode: accept_node})
+    return tree_walker;
+  }
+
+  static is_script_node(node: Node): boolean {
+      return ["SCRIPT", "STYLE"].includes((node.parentElement as Node).nodeName);
+  }
+
+  static is_text_node_checked(node: Node): boolean {
+    if (node.parentElement) {
+      return (node.parentElement as HTMLElement).classList.contains("nsfw");
+    }
+    return false
+  }
+
+  static is_text_node_empty(node: Node): boolean {
+    return (node as CharacterData).data.replace(/\s/g, "").length  <= 1;
+  }
 }
 
-window.onload = (event: Event) => {
+window.addEventListener("DOMContentLoaded", (event: Event) => {
   let nsfw_words_filter = new NSFWWordsFilter();
   nsfw_words_filter.filter();
-}
-
+  nsfw_words_filter.watch();
+});
